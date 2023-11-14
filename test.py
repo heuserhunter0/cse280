345cc4c777fd8890d4b1dfbd112587905d87aa1a
heuserhunter0 <87386401+heuserhunter0@users.noreply.github.com>
2023-11-14 14:17:07 -0700

    Finished prove 10 and tested the python code.

--- test.py
+++ test.py
@@ -1,12 +1,23 @@
-seq1 = [k**3 for k in range(1,54)]
+def gcd(x,y):
+    for n in range(min(x,y),1,-1):
+        if x % n == 0 and y % n == 0:
+            return n
+    return 1
 
-seq2 = [2**k - 2 for k in range(4, 28)]
+def lcm(x, y):
+    return (x * y) // gcd(x, y)
 
-seq3 = [k**5 + 1 for k in range(-3, 19)]
 
-seq4 = [2**k for k in range(-2, 8)]
-
-print(sum(seq1)) # 2047761
-print(sum(seq2)) # 268435392
-print(sum(seq3)) # 6656947
-print(sum(seq4)) # 255.75
+print(gcd(12,15)) # 3
+print(gcd(12,24)) # 12
+print(gcd(12,18)) # 6 
+print(gcd(7,13))  # 1
+print(gcd(1,2))   # 1
+print(gcd(5,5))   # 5
+print("===================")
+print(lcm(12,15)) # 60
+print(lcm(12,24)) # 24
+print(lcm(12,18)) # 36 
+print(lcm(7,13))  # 91
+print(lcm(1,2))   # 2
+print(lcm(5,5))   # 5
